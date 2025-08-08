2fde51d044a2b4b26a07562f714bf8328004d46b
Sujan-2006 <sujan032006@gmail.com>
2025-08-08 21:09:08 +0530

    adminDashboard.dart ui updated

--- lib/Admin/facultyControl.dart
+++ lib/Admin/facultyControl.dart
@@ -179,10 +179,18 @@ class _FacultyOverviewPageState extends State<FacultyOverviewPage> {
                             ),
                             ElevatedButton(
                               child: const Text("Delete"),
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.red),
                               onPressed: () => Navigator.pop(context, true),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xFFFF7F50), // Button color
+                                foregroundColor: Colors.white, // Text color
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10), // Rounded corners
+                                ),
+                                padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20), // Padding
+                              ),
                             ),
+
+
                           ],
                         ),
                       );
@@ -200,7 +208,7 @@ class _FacultyOverviewPageState extends State<FacultyOverviewPage> {
                       }
                     },
                     child: const Text("Delete",
-                        style: TextStyle(color: Colors.red)),
+                        style: TextStyle(color: Color(0xFFFF7F50))),
                   ),
                   const Spacer(),
                   ElevatedButton(
@@ -527,7 +535,7 @@ class _FacultyOverviewPageState extends State<FacultyOverviewPage> {
                 padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
                 decoration: const BoxDecoration(
                   border: Border(
-                    bottom: BorderSide(color: Colors.orange, width: 1),
+                    bottom: BorderSide(color: Color(0xFFFF7F50), width: 1),
                   ),
                 ),
                 child: Row(
@@ -577,7 +585,8 @@ class _FacultyOverviewPageState extends State<FacultyOverviewPage> {
             IconButton(
               icon: Image.asset("assets/homeLogo.png", height: 32),
               onPressed: () {
-                Navigator.popUntil(context, ModalRoute.withName("/admin_dashboard"));
+                Navigator.popUntil(
+                    context, ModalRoute.withName("/admin_dashboard"));
               },
             ),
             IconButton(
